22d55ffaf3f8a0a00d93090653127f891271cd91
Mohamed Hamdallah <moh8med@gmail.com>
2018-01-27 22:13:23 +0200

    Added the ability to create users.

--- Http/Controllers/UserController.php
+++ Http/Controllers/UserController.php
@@ -5,8 +5,8 @@ namespace Modules\User\Http\Controllers;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-// use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\ValidationException;
 use Modules\User\Entities\User;
 
 class UserController extends Controller
@@ -30,6 +30,54 @@ class UserController extends Controller
             : User::orderBy('id', 'desc');
 
         return json_response()->paginate($data->paginate(per_page())->toArray());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        // Authorization check
+        if (Gate::denies('create', User::class)) {
+            return json_response()->forbidden();
+        }
+
+        // Validate request data
+        $data = $request->validate([
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'username' => 'sometimes|nullable|required|string|unique:users',
+            'email' => 'required|string|email|max:150|unique:users',
+            'password' => 'required|string|min:6|max:255|confirmed',
+            'access_level' => 'sometimes|integer',
+            'is_active' => 'sometimes|boolean',
+        ]);
+
+        // Create new resource
+        $resource = new User;
+        $resource->fill($data);
+
+        // Encrypt user password
+        $resource->password = bcrypt($data['password']);
+
+        // Set user status & access level
+        $resource->is_active = $request->input('is_active') ?: 0;
+        $resource->access_level = $request->input('access_level') ?: 0;
+
+        if ($resource->save()) {
+            // Send welcome email
+            // dispatch(new SendWelcomeEmail($resource));
+
+            return json_response()->success([
+                'messages' => ['A new resource has been created successfully.'],
+                'resource' => User::find($resource->id),
+            ], 201);
+        }
+
+        return json_response()->internalServerError();
     }
 
     /**
