0e4d0f528ceeb1ba3315bbf69d56418b3b8e6738
Mohamed Hamdallah <moh8med@gmail.com>
2018-01-27 23:11:26 +0200

    Added the ability to update users.

--- Http/Controllers/UserController.php
+++ Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
 use Modules\User\Entities\User;
 
@@ -49,7 +50,7 @@ class UserController extends Controller
         $data = $request->validate([
             'first_name' => 'required|string',
             'last_name' => 'required|string',
-            'username' => 'sometimes|nullable|required|string|unique:users',
+            'username' => 'sometimes|nullable|required|string|max:50|unique:users',
             'email' => 'required|string|email|max:150|unique:users',
             'password' => 'required|string|min:6|max:255|confirmed',
             'access_level' => 'sometimes|integer',
@@ -106,6 +107,87 @@ class UserController extends Controller
         }
 
         return json_response()->success($data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+        // Get resource
+        try {
+            $resource = User::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return json_response()->notFound();
+        }
+
+        // Authorization check
+        if (Gate::denies('update', $resource)) {
+            return json_response()->forbidden();
+        }
+
+        if (($request->has('access_level') || $request->has('is_active')) && ! app('auth')->user()->is_admin) {
+            return json_response()->forbidden();
+        }
+
+        // Validate request data
+        $data = $request->validate([
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'username' => [
+                'sometimes',
+                'nullable',
+                'required',
+                'string',
+                'max:50',
+                Rule::unique('users')->ignore($id),
+            ],
+            'email' => [
+                'sometimes',
+                'required',
+                'string',
+                'email',
+                'max:150',
+                Rule::unique('users')->ignore($id),
+            ],
+            'password' => 'sometimes|required|string|min:6|max:255|confirmed',
+            'access_level' => 'sometimes|integer',
+            'is_active' => 'sometimes|boolean',
+        ]);
+
+        // Update resource
+        $resource->fill($data);
+
+        // Set new password if present
+        if ($request->has('password')) {
+            $resource->password = bcrypt($data['password']);
+        }
+
+        // Set user status & access level
+        if ($request->has('is_active')) {
+            $resource->is_active = $request->input('is_active');
+        }
+        if ($request->has('access_level')) {
+            $resource->access_level = $request->input('access_level');
+        }
+
+        if ($resource->save()) {
+            // Send password changed
+            // if ($request->has('password')) {
+            //     dispatch(new SendPasswordChangedEmail($resource));
+            // }
+
+            return json_response()->success([
+                'messages' => [trans('messages.resource_updated')],
+                'resource' => User::find($resource->id),
+            ]);
+        }
+
+        return json_response()->internalServerError();
     }
 
     /**
